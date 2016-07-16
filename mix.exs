a0abfc967ac478a9b882b7c7c8df9735106e57d2
Dylan Kendal <dylan@stembolt.com>
2016-07-16 09:32:28 -0700

    Added reprise for code reloading

--- mix.exs
+++ mix.exs
@@ -17,7 +17,24 @@ defmodule Snek.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {Snek, []},
-     applications: [:phoenix, :phoenix_pubsub, :cowboy, :logger, :gettext]]
+     applications: applications(Mix.env)
+   ]
+  end
+
+  def applications(:dev) do
+    [
+      :reprise,
+    ] ++ applications(:all)
+  end
+
+  def applications(_) do
+    [
+      :cowboy,
+      :gettext,
+      :logger,
+      :phoenix,
+      :phoenix_pubsub,
+    ]
   end
 
   # Specifies which paths to compile per environment.
@@ -28,9 +45,12 @@ defmodule Snek.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.0"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+    [
+      {:cowboy, "~> 1.0"},
+      {:gettext, "~> 0.11"},
+      {:phoenix, "~> 1.2.0"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:reprise, "~> 0.5", only: :dev}
+    ]
   end
 end
